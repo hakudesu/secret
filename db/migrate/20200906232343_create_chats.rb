e83896fb33d18850f441ab74fb606eb836d8d169
EC2 Default User <ec2-user@ip-172-31-10-148.ap-northeast-1.compute.internal>
2020-09-06 23:26:54 +0000

    chat マイグレーションファイル記述

--- db/migrate/20200906232343_create_chats.rb
+++ db/migrate/20200906232343_create_chats.rb
@@ -1,6 +1,9 @@
 class CreateChats < ActiveRecord::Migration[5.2]
   def change
     create_table :chats do |t|
+      t.string :name
+      t.text :text
+      t.text :image
 
       t.timestamps
     end
