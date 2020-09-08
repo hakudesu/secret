cc5cdb4365e06c7cd3d30818ccfb9806c485c912
EC2 Default User <ec2-user@ip-172-31-10-148.ap-northeast-1.compute.internal>
2020-09-08 02:02:13 +0000

    chatscontrollerにcreateメソッド記述

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -4,4 +4,14 @@ class ChatsController < ApplicationController
         @chats = Chat.all
     end
     
+    def create
+        Chat.create(chat_params)
+    end
+    
+    private
+    def chat_params
+        params.permit(:name, :text, :image )
+    end
+    
+    
 end
